62da4522ef4cec0f1a09011dd228071269cbd0a1
jyand <jyand@protonmail.com>
2020-11-25 00:36:34 +0000

    expanding on some boilerplate

--- index.ts
+++ index.ts
@@ -1,8 +1,25 @@
 let map: google.maps.Map ;
 let iw: google.maps.InfoWindow ;
 let svc: google.maps.places.PlacesService ;
+const defaultLocation = new google.maps.LatLng(, ) ;
 
-function initMap(): void {
-        map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: -34.397, lng: 150.644 }, zoom: 8, }) ;
+class request {
+        query: string ,
+        fields =  ['name', 'geometry'],
+}
+
+function initMap(searchTerm:string): void {
+        iw = new google.maps.InfoWindow() ;
+        map = new google.maps.Map(document.getElementById("map") as HTMLElement, {center: defaultLocation, zoom: 8}) ;
+        svc = new google.maps.places.PlacesService(map) ;
+
+        var q = new request(searchTerm,) ;
+        svc.findPlaceFromQuery(q, function(results, status)) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        for (let i:number = 0 ; i < results.length ; ++i) {
+                                createMarker(results[i]) ;
+                        }
+                        map.SetCenter(results[0].geometry.location) ;
+                }
+        }
 }
